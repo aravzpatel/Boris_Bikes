b1769488eb49bde58f884c565e98144dc13dbaa1
James Forster <jamesalexforster@gmail.com>
2020-06-09 16:02:10 +0100

    creates dockingstation test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,3 +1,16 @@
+require 'docking_station.rb'
+
 describe DockingStation do
-  
+  it 'releases bike' do
+    expect(DockingStation).to respond_to :release_bike
+  end
 end
+
+
+# describe 'Greeter' do
+#   it 'greets Rico' do
+#     expect(greet('Rico')).to eq 'Hello, Rico, how are you today?'
+#   end
+# end
+
+# expect(obj).to respond_to :save!
